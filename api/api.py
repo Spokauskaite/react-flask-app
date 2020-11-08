96a9ab66593b36cd72681cc0ad26b165e11ae9a9
Spokauskaite <53835940+Spokauskaite@users.noreply.github.com>
2020-11-07 20:02:25 -0500

    add db call

--- api/api.py
+++ api/api.py
@@ -4,11 +4,62 @@ Created on Sat Nov  7 09:35:46 2020
 
 @author: Lina
 """
-import time
+import sqlite3
+import logging
+from logging import Formatter, FileHandler
 from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/time')
-def get_current_time():
-    return {'time': time.time()}
+# this is for logging ---------------------------
+LOGGER = logging.getLogger('whatever')
+file_handler = FileHandler('test.log')
+handler = logging.StreamHandler()
+file_handler.setFormatter(Formatter(
+    '%(asctime)s %(levelname)s: %(message)s '
+    '[in %(pathname)s:%(lineno)d]'
+))
+handler.setFormatter(Formatter(
+    '%(asctime)s %(levelname)s: %(message)s '
+    '[in %(pathname)s:%(lineno)d]'
+))
+LOGGER.addHandler(file_handler)
+LOGGER.addHandler(handler)
+LOGGER.setLevel(logging.INFO)
+#------------------------------------------------
+
+def fetchFromDB(sql_query):
+    database_file =  'food_data.db'
+    conn = sqlite3.connect(database_file)
+    c = conn.cursor()
+    # fetch id 
+    c.execute(sql_query)
+    fetchedItem = c.fetchone()
+    fetchedItem = fetchedItem[0]
+    LOGGER.info('Returning DB Item:')
+    LOGGER.info(fetchedItem)
+    return (fetchedItem)
+
+@app.route('/food/<name>')
+def getFood(name):
+    # fetch id 
+    sql_query = "SELECT fdc_id FROM food WHERE description = '{}'".format(name)
+    fetched_id = fetchFromDB(sql_query)
+    LOGGER.info('Returned DB Item:')
+    LOGGER.info(fetched_id)
+    return {'id':fetched_id}
+
+@app.route('/nutrient/<int:id>')
+def getNutrient(id):
+    # fetch name
+    sql_query = "SELECT name FROM nutrient WHERE id = {}".format(id)
+    fetched_name = fetchFromDB(sql_query)
+    LOGGER.info('Returned DB Item:')
+    LOGGER.info(fetched_name)
+    return {'name':fetched_name}
+
+# this is for logging-------------------------
+if __name__ == '__main__':
+    app.run()
+
+
--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+  const [foodItem, setFoodItem] = useState(null);
+  const [nutrientItem, setNutrientItem] = useState(null);
+
+  useEffect(() => {
+
+    const foodName = "Metabolizable Energy of Almonds"
+    const nutrientId = 1002
+
+    fetch(`/food/${foodName}`).then(res => res.json()).then(data => {
+      setFoodItem(data.id)
+    }); 
+
+    fetch(`/nutrient/${nutrientId}`).then(res => res.json()).then(data => {
+      setNutrientItem(data.name)
+    }); 
+
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div>
+          {foodItem === null ? 
+            <div style={{ margin: '20px' }}>Please wait, fetching food ID by name...</div> :
+            <div style={{ margin: '20px' }}>Food Item ID fetched by name 'Metabolizable Energy of Almonds' is: {foodItem}</div>
+          }  
+        </div> 
+        <div>
+          {nutrientItem === null ? 
+            <div style={{ margin: '20px' }}>Please wait, fetching nutrient name by id...</div> :
+          <div style={{ margin: '20px' }}>Nutrient Item Name fetched by ID 1002 is: {nutrientItem}</div>
+          }  
+        </div> 
     </div>
   );
 }
